55a18c5d5674ab76155be5a2df99defdf07397bd
xyerakovalskia <xyera123987@gmail.com>
2026-03-30 18:43:37 +0700

    looping_3

--- looping.cpp
+++ looping.cpp
@@ -24,3 +24,17 @@ int main() {
         x = rand()%10;
     }
 
+    cout<<"Bilangan acak while yang terakhir ="<<x <<endl;
+    cout<<endl;
+
+    //Perulangan dengan DO...WHILE
+    cout<<" PERULANGAN DO...WHILE"<<endl;
+    x = rand()%10;
+    do{
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }while(x<=5);
+
+    cout<<"Bilangan acak do-while yang terakhir ="<<x <<endl;
+    cout<<endl;
+}
