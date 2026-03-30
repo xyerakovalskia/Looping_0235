0f22a650bdd5d424bc91b3bfb3d4e3865941acf9
xyerakovalskia <xyera123987@gmail.com>
2026-03-30 19:01:43 +0700

    looping_6

--- looping.cpp
+++ looping.cpp
@@ -52,3 +52,12 @@ int main() {
     cout<<"Masukkan nilai Bahasa Inggris = ";
     cin>>nilB;
 
+    rerata = (nilB+nilM)/2;
+
+    if (!(rerata < 60) )
+        status = "Lulus";
+    else
+        status = "Tidak Lulus";
+
+    cout<<"Status kelulusan = "<<status<< ",dengan nilai rata rata = "<<rerata<<endl;
+}
